2d542a932b03c857c1e6e02380d3e17b937d85a1
hjtunen <91310858+hjtunen@users.noreply.github.com>
2021-12-10 12:58:36 +0200

    4.15

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const logger = require("./utils/logger")
 const config = require("./utils/config")
 const blogsRouter = require("./controllers/blogs")
 const middleware = require("./utils/middleware")
+const usersRouter = require("./controllers/users")
 
 logger.info("connecting to", config.MONGODB_URI)
 
@@ -24,6 +25,7 @@ app.use(express.json())
 app.use(middleware.requestLogger)
 
 app.use("/api/blogs", blogsRouter)
+app.use("/api/users", usersRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
